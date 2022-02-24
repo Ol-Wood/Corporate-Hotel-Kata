b3e1f06a7da286ac4388894ad9934269300ddc5a
oawood93 <oa.wood@outlook.com>
2022-02-24 10:30:36 +0000

    failing for the right reason

--- src/test/java/BookingFeature.java
+++ src/test/java/BookingFeature.java
@@ -1,10 +1,12 @@
 import com.example.domain.*;
+import com.example.domain.exceptions.BookingNotAllowedException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookingFeature {
 
@@ -27,5 +29,15 @@ public class BookingFeature {
 
         Booking result = bookingService.book(employeeID, new HotelID(1), roomType, checkinDate, checkoutDate);
         assertNotNull(result);
+    }
+
+    @Test
+    void should_throw_booking_not_allowed_exception_if_policy_doesnt_allow_booking(){
+        var employeeID = new EmployeeID(1);
+        var roomType = RoomType.DEFAULT;
+        bookingPolicyervice.setEmployeePolicy(employeeID, roomType);
+        var checkinDate = LocalDateTime.now();
+        var checkoutDate = LocalDateTime.now();
+        assertThrows(BookingNotAllowedException.class, () -> bookingService.book(employeeID, new HotelID(1), roomType, checkinDate, checkoutDate));
     }
 }
